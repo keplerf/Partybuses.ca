3796b9ab1d5d0a80104dad55592b192254887750
Kepler Fontenele <>
2019-04-22 15:51:05 -0700

    DEMO git comment

--- 404.php
+++ 404.php
@@ -9,25 +9,23 @@
 
 get_header(); ?>
 
-
 		<div id="content" role="main">
-
 			<article id="post-0" class="post error404 not-found">
 				<header class="entry-header">
 					<h1 class="entry-title"><?php _e( 'This is somewhat embarrassing, isn&rsquo;t it?', 'twentyeleven' ); ?></h1>
 				</header>
-                
-                 
+
+
 				<div id="products">
-					
 
-					
 
-                    
+
+
+
                     <section class="primary">
 						<?php the_widget( 'WP_Widget_Tag_Cloud' ); ?>
                     </section>
-                    
+
                     <section class="secundary">
 						<?php the_widget( 'WP_Widget_Recent_Posts', array( 'number' => 3 ), array( 'widget_id' => '404' ) ); ?>
 					</section>
@@ -35,12 +33,12 @@ get_header(); ?>
 				</div><!-- .entry-content -->
 			</article><!-- #post-0 -->
 
-		
-        
-         <?php get_template_part( 'content-tab' ); ?>	
-         
+
+
+         <?php get_template_part( 'content-tab' ); ?>
+
           <?php get_template_part( 'content-footer1' ); ?>
-          
+
           </div><!-- #content -->
-	
+
 <?php get_footer(); ?>
